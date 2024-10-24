d2267909165f154fc8062570ec0ed58045259bdc
amar-qa <v-amarnathms@microsoft.com>
2024-10-24 02:29:01 +0000

    Added Api Code

--- api/src/functions/Endpoint.js
+++ api/src/functions/Endpoint.js
@@ -1,6 +1,6 @@
 const { app } = require('@azure/functions');
 
-app.http('httpTrigger', {
+app.http('Endpoint', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
